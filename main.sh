aee08c30aadd39697112c5cecb95cc8835a0c5f7
LakeVostok <lakevostok@yandex.ru>
2025-07-16 23:49:45 +0300

    remove unused file

--- main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-IFS=$' \t\r\n'
-
-source /etc/app_creds
-
-source ~/availability-checker/check.sh
-
-check_flow --alarm_chat_id="$ALARM_CHAT_ID" --log_chat_id="$LOG_CHAT_ID" --bot_token="$BOT_TOKEN" --uri_file="./list"
